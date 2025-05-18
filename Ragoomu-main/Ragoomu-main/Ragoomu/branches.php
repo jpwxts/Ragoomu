8021121f8785d2d53c51c7a462d3c7cb6adeb035
JoshRayKong <joshuaray.kong@wvsu.edu.ph>
2025-05-19 01:00:38 +0800

    Add files via upload

---
+++ Ragoomu-main/Ragoomu-main/Ragoomu/branches.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ragoomu | Branches</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+    <link rel="icon" href="images/Ragoomu [Logo].png" sizes="32x32" type="image/png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1><img class="img-logo" src="images/Ragoomu [Logo].png" alt="Logo">
+            <img class="img-namelogo" src="images/Ragoomu [Name Logo].png" alt="Namelogo"></h1>
+        <nav id="main-nav">
+            <div class="topnav"><ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="branches.php">Branches</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul></div>
+        </nav>
+        <form id="search-form" onsubmit="return handleSearch(event)">
+            <input type="text" id="search-box" placeholder="What are you looking for?">
+            <button type="submit">Search</button>
+        </form>
+    </header>
+    <main>
+        <h2>Find a nearby Ragoomu today! We might be closer than you think!</h2>
+        <div class="Branchbody"><p>Ragoomu West-Nasyo<br><img src="images/luna_st.jpg" alt=""> <br> Ragoomu Dungon<br><img src="images/dungon.jpg" alt=""><br> Ragoomu Ledesco<br><img src="images/ledesco.jpg" alt=""></p></div>
+    </main>
+    <footer>
+        <p>&copy; 2025 Ragoomu. All rights reserved.</p>
+    </footer>
+</body>
+</html>
---
+++ Ragoomu-main/Ragoomu-main/Ragoomu/script.js
@@ -0,0 +1,130 @@
+// login settings
+function showForm(formId) {
+    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
+    document.getElementById(formId).classList.add("active");
+}
+// end of login settings
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pages = [
+        { name: "home page", url: "home.php" },
+        { name: "about us", url: "about.php" },
+        { name: "products", url: "products.php" },
+        { name: "branches", url: "branches.php" },
+        { name: "contact us", url: "contact.php" },
+    ];
+
+    window.handleSearch = (event) => {
+        event.preventDefault();
+        const query = document.getElementById("search-box").value.toLowerCase();
+
+        const page = pages.find(p => p.name === query);
+        if (page) {
+            window.location.href = page.url;
+        } else {
+            alert("Page not found. Please try searching for 'Home Page', 'About Us', 'Products', 'Branches', or 'Contact Us'.");
+        }
+
+        
+    };
+});
+
+let openOrder = document.querySelector('.order');
+let closeOrder = document.querySelector('.closeOrder');
+let list = document.querySelector('.list');
+let listCart = document.querySelector('.listCart');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openOrder.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeOrder.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'ANGUS CHEESE BURGER',
+        image: 'Angus_Cheese.jpg',
+        price: 169
+    },
+    {
+        id: 2,
+        name: 'CLASSIC CHICKEN BURGER',
+        image: 'Classic_Chicken.jpg',
+        price: 139
+    },
+    {
+        id: 3,
+        name: 'QUESSO BBQ',
+        image: 'Quesso_BBQ.jpg',
+        price: 245
+    },
+    {
+        id: 4,
+        name: 'SNOW CHEESE',
+        image: 'Snow_Cheese.jpg',
+        price: 245
+    },
+
+];
+let listCarts  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCart(${key})">Add To Order</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCart(key){
+    if(listCarts[key] == null){
+        // copy product form list to list cart
+        listCarts[key] = JSON.parse(JSON.stringify(products[key]));
+        listCarts[key].quantity = 1;
+    }
+    reloadCart();
+}
+function reloadCart(){
+    listCart.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCarts.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCart.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCarts[key];
+    }else{
+        listCarts[key].quantity = quantity;
+        listCarts[key].price = quantity * products[key].price;
+    }
+    reloadCart();
+}
+
